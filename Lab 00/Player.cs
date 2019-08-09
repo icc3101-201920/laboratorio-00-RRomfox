1d58c7e6d4651ee8609378c155e785585fe9125d
Felipe Román <you@example.com>
2019-08-09 09:46:56 -0400

    Base3

--- Lab 00/Player.cs
+++ Lab 00/Player.cs
@@ -6,6 +6,11 @@ namespace Card
 {
     public class Player
     {
+        private int lifePoints;
+        private int attackPoints;
+
+        public int LifePoints { get => lifePoints; set => lifePoints = value; }
+        public int AttackPoints { get => attackPoints; set => attackPoints = value; }
 
 
     }
